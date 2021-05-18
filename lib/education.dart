c36999e84fd2c603756fc7367185394d029ac60a
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2021-05-18 12:22:41 +0300

    LOL2

---
+++ lib/education.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class Education extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.white, //change your color here
+        ),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        title: Text("Education", style: GoogleFonts.inconsolata(color: Colors.white)),
+      ),
+      body: Center(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 1000,
+                  child: Card(
+                    color: Colors.black,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        side: BorderSide(color: Colors.white)
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          leading: FaIcon(FontAwesomeIcons.school, color: Colors.white),
+                          title: Text("ALEV Schools (Primary and Middle School)", style: GoogleFonts.inconsolata(color: Colors.white),),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            TextButton(
+                              child: Text("2010 - 2018", style: GoogleFonts.inconsolata(color: Colors.white, fontSize: 16)),
+                              onPressed: () {},
+                            ),
+                            SizedBox(width: 8),
+                          ],
+                        ),
+                        Divider(color: Colors.white),
+                        ListTile(
+                          leading: FaIcon(FontAwesomeIcons.school, color: Colors.white),
+                          title: Text("TED Istanbul Koleji (High School)", style: GoogleFonts.inconsolata(color: Colors.white),),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            TextButton(
+                              child: Text("2019 - 2023", style: GoogleFonts.inconsolata(color: Colors.white, fontSize: 16)),
+                              onPressed: () {},
+                            ),
+                            SizedBox(width: 8),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+}
