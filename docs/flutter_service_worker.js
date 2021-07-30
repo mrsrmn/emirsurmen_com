2c93b9e859bc795f40ee9d3cc25989ef0c5f8194
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2021-07-30 12:32:44 +0300

    help me

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -17,8 +17,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "cab4e1e801b24026df7fcff47f1a25ad",
-"/": "cab4e1e801b24026df7fcff47f1a25ad",
+"index.html": "7f3d50c8928e834bb81f4848c7393455",
+"/": "7f3d50c8928e834bb81f4848c7393455",
 "main.dart.js": "c94954897cf8b2bec75c49ea3f60193e",
 "manifest.json": "e538d147dc8ebac7b4058e1eb57e849d",
 "version.json": "68db465ff52f2cea78cd5b2b4848c009"
