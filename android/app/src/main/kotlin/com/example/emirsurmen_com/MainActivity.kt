0def39033e94473fa76ef5726fe973c654287730
Emir <emirsurmen@gmail.com>
2023-11-04 20:06:12 +0300

    hugoo

--- android/app/src/main/kotlin/com/example/emirsurmen_com/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.emirsurmen_com
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:url_strategy/url_strategy.dart';
-
-import 'app.dart';
-
-void main() {
-  setPathUrlStrategy();
-  runApp(MyApp());
-}
